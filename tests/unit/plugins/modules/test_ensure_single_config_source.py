799deb7b1daac3e7915f95c979115dc21a894e51
Mack (Maksym Iv) <maksym@elastiflow.com>
2025-09-04 20:12:19 +0300

    fix: Tests compatibility with Ansible pre v2.19

--- tests/unit/plugins/modules/test_ensure_single_config_source.py
+++ tests/unit/plugins/modules/test_ensure_single_config_source.py
@@ -1,10 +1,18 @@
 import json
 
 import pytest
-from ansible.module_utils import basic, testing
+from ansible.module_utils import basic
 from ansible.module_utils.common.text.converters import to_bytes
 
 from plugins.modules.ensure_single_config_source import get_by_dot, run_module, valid
+
+# "testing" is present in Ansible 2.19+
+try:
+    from ansible.module_utils import testing
+
+    ansible_new = True
+except ImportError:
+    pass
 
 
 def set_module_args(args):
@@ -97,13 +105,26 @@ def test_valid(vals, path, want):
 def test_run_module(mocker, config_values, config_path, ansible_vars, want):
     mocked_fail = mocker.patch.object(basic.AnsibleModule, "fail_json")
     mocker.patch.object(basic.AnsibleModule, "exit_json")
-    with testing.patch_module_args(
-        {
-            "config_values": config_values,
-            "config_path": config_path,
-            "vars": ansible_vars,
-        },
-    ):
+
+    if ansible_new:
+        with testing.patch_module_args(
+            {
+                "config_values": config_values,
+                "config_path": config_path,
+                "vars": ansible_vars,
+            },
+        ):
+            run_module()
+            assert mocked_fail.call_count == want
+    else:
+        set_module_args(
+            {
+                "config_values": config_values,
+                "config_path": config_path,
+                "vars": ansible_vars,
+            },
+        )
+
         run_module()
         assert mocked_fail.call_count == want
 
@@ -136,14 +157,26 @@ def test_run_module(mocker, config_values, config_path, ansible_vars, want):
     ],
 )
 def test_run_module_exception(config_values, config_path, ansible_vars, want):
-    with (
-        testing.patch_module_args(
+    if ansible_new:
+        with (
+            testing.patch_module_args(
+                {
+                    "config_values": config_values,
+                    "config_path": config_path,
+                    "vars": ansible_vars,
+                },
+            ),
+            pytest.raises(want),
+        ):
+            run_module()
+    else:
+        set_module_args(
             {
                 "config_values": config_values,
                 "config_path": config_path,
                 "vars": ansible_vars,
             },
-        ),
-        pytest.raises(want),
-    ):
-        run_module()
+        )
+
+        with pytest.raises(want):
+            run_module()
